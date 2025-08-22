73ead718fa5c6e4911e929c08948f1625155ec51
Pra-Git7 <Gupta.kashpra@gmail.com>
2025-08-22 11:37:16 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,7 +1,7 @@
 <?php
 include "connection.php";
 
-// Collecting form data
+
 $ref = $_POST['ref'] ?? null;
 $date = $_POST['date'] ?? null;
 $client = $_POST['client'] ?? null;
@@ -9,7 +9,6 @@ $site_name = $_POST['site_name'] ?? null;
 $gst = $_POST['gst'] ?? null;
 $invoice_date = $_POST['invoice_date'] ?? null;
 
-// Prepare statement to prevent SQL injection
 $stmt = $conn->prepare("INSERT INTO `project`(`ref`, `date`, `client`, `site_name`, `gst`, `invoice_date`) VALUES (?, ?, ?, ?, ?, ?)");
 $stmt->bind_param("ssssss", $ref, $date, $client, $site_name, $gst, $invoice_date);
 
@@ -21,4 +20,5 @@ if ($stmt->execute()) {
 
 $stmt->close();
 $conn->close();
+
 ?>
